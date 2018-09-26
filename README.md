fe8d034df19ee6b8e85d8a448aad077f23dbb6b2
Magrit <mikael.pettersson201@gmail.com>
2018-09-26 21:39:12 +0200

    Loops until we clear the buffer!

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # vscp_interfacelib
 Best vscpinterface ever
+
+This is how to use it:
+----
+fn main() {
+
+let host = "10.0.0.45:50001";
+let mut client = Client::new(host.to_owned());
+let mut buf : [u8; 12] = [0; 12];
+loop
+{
+    let packet = client.read_vscp(&mut buf);
+    println!("Values sent are:  forward_backward:{:?}, left_right:{:?}", packet.forward_backward, packet.left_right);
+}
+
+#Client
+
+An UDP client that receives packets
+
+
+# Packet
+
+The packet struct is defined as:
+forward_backward : f32
+left_right : f32
--- src/lib.rs
+++ src/lib.rs
@@ -17,24 +17,19 @@ pub left_right :f32
 
 impl Packet
 {   
-    pub fn deserialize(cursor : &mut Cursor<&mut[u8]>) -> Packet
+    pub fn deserialize(cursor : &mut Cursor<&mut[u8]>) -> Result<Packet, &'static str>
     {
         let magic_u32 = cursor.read_u32::<LittleEndian>().unwrap();
         if magic_u32 == 0xAABBCCDD 
         {
-            Packet
+            Ok(Packet
             {
                 forward_backward:cursor.read_f32::<LittleEndian>().unwrap(),
                 left_right:cursor.read_f32::<LittleEndian>().unwrap()
-            }
+            })
         }else
         {
-            println!("ERROR: INVALID MAGIC!");
-            Packet
-            {
-                forward_backward: 0.0,
-                left_right: 0.0
-            }
+            Err("ERROR: INVALID MAGIC!")
         }
     }
 }
@@ -62,33 +57,30 @@ impl Client
 
     pub fn read_vscp(&mut self, buf : &mut [u8; 12]) -> Packet
     {
-        let result = self.sock.recv(buf);
-        match result {
-            Ok(num_bytes) => {
-                let mut cursor = Cursor::new(&mut buf[..num_bytes]);
-                self.last_packet = Packet::deserialize(&mut cursor);
-                self.last_packet
-            },
-            Err(ref err) if err.kind() != ErrorKind::WouldBlock => {
-                println!("Something went wrong: {}", err);
-                self.last_packet
-            }, 
-            _ => {
-                self.last_packet
+        let mut newdata = true;
+        while newdata
+        {
+            let result = self.sock.recv(buf);
+            match result {
+                Ok(num_bytes) => {
+                    let mut cursor = Cursor::new(&mut buf[..num_bytes]);
+                    let packet_result = Packet::deserialize(&mut cursor);
+                    match packet_result {
+                        Ok(packet) => 
+                        {
+                            self.last_packet = packet;
+                        },
+                        Err(str) => {println!("Error: {}", str)}
+                    }
+                },
+                Err(ref err) if err.kind() != ErrorKind::WouldBlock => {
+                    println!("Something went wrong: {}", err);
+                }, 
+                _ => {
+                    newdata = false;
+                }
             }
         }
+        self.last_packet
     }
 }
-
-// fn main() {
-
-// let host = "10.0.0.45:50001";
-// let client = Client::new(host.to_owned());
-// let mut buf : [u8; 12] = [0; 12];
-// loop
-// {
-//     let packet = client.read_vscp(&mut buf);
-//     println!("Values sent are:  forward_backward:{:?}, left_right:{:?}", packet.forward_backward, packet.left_right);
-// }
-
-// }
