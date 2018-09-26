676de4208f1a4edad662a12a62278c6d5c91e7a6
Magrit <mikael.pettersson201@gmail.com>
2018-09-26 21:15:51 +0200

    Testaddition of nonblocking

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 
 extern crate byteorder;
 
-pub mod vscp{
-
 use std::io::Cursor;
 use std::net::UdpSocket;
 use std::string::String;
+use std::io::ErrorKind;
 use byteorder::{ReadBytesExt, LittleEndian};
 
+#[derive (Copy)]
 pub struct Packet {
-    // magic : u32 == 0xAABBCCDD
-    pub forward_backward : f32,
-    pub left_right :f32
+// magic : u32 == 0xAABBCCDD
+pub forward_backward : f32,
+pub left_right :f32
 }
 
 
@@ -39,26 +39,43 @@ impl Packet
     }
 }
 
-    pub struct Client
+pub struct Client
+{
+    sock : UdpSocket,
+    last_packet : Packet
+}
+
+impl Client
+{
+    pub fn new(host : String) -> Client
     {
-        sock : UdpSocket
+        let sock = UdpSocket::bind(host).expect("Could not bind hostname");
+        sock.set_nonblocking(true)
+        .expect("Failed to enter non-blocking mode");
+        Client {
+            sock: sock,
+            last_packet: Packet{
+                forward_backward: 0.0,
+                left_right: 0.0}
+        }
     }
 
-    impl Client
+    pub fn read_vscp(&mut self, buf : &mut [u8; 12]) -> Packet
     {
-        pub fn new(host : String) -> Client
-        {
-            Client {
-                sock: UdpSocket::bind(host).expect("Could not bind hostname: {:?}")
+        let result = self.sock.recv(buf);
+        match result {
+            Ok(num_bytes) => {
+                let mut cursor = Cursor::new(&mut buf[..num_bytes]);
+                self.last_packet = Packet::deserialize(&mut cursor);
+                self.last_packet
+            },
+            Err(ref err) if err.kind() != ErrorKind::WouldBlock => {
+                println!("Something went wrong: {}", err);
+                self.last_packet
+            }, 
+            _ => {
+                self.last_packet
             }
-        }
-
-        pub fn read_vscp(&self, buf : &mut [u8; 12]) -> Packet
-        {
-            let number_of_bytes = self.sock.recv(buf)
-            .expect("Did not receive data!");
-            let mut cursor = Cursor::new(&mut buf[..number_of_bytes]);
-            Packet::deserialize(&mut cursor)
         }
     }
 }
